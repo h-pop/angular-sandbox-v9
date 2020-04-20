6f87560368b56ab9d0f47291b7803e80920cba0c
hpopiolkiewicz <h.popiolkiewicz@gmail.com>
2020-04-20 15:54:40 +0200

    Finished section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'training-project-v9';
+export class AppComponent implements OnInit, OnDestroy {
+
+  userActivated = false;
+  activatedSubscription: Subscription;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.activatedSubscription = this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    });
+  }
+
+  ngOnDestroy() {
+    this.activatedSubscription.unsubscribe();
+  }
 }
