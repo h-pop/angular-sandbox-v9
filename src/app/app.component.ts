6b516fdc223b3ab944bb809600cdb752e35033e0
Hubert Popiolkiewicz <popiolkiewicz.it@gmail.com>
2020-04-22 10:39:55 +0200

    Finished reactive form assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'training-project-v9';
+export class AppComponent implements OnInit {
+  customFormGroup: FormGroup;
+  projectStatuses: string[] = ['Stable', 'Critical', 'Finished'];
+
+  onSubmit() {
+    console.log(this.customFormGroup);
+  }
+
+  ngOnInit() {
+    this.customFormGroup = new FormGroup({
+      projectName: new FormControl('', [Validators.required/*, this.forbiddenProjectNames */], this.forbiddenProjectNamesAsync),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      projectStatus: new FormControl('')
+    });
+  }
+
+  forbiddenProjectNames(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Test') {
+      return { projectNameForbidden: true };
+    }
+    return null;
+  }
+
+  forbiddenProjectNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ projectNameForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
 }
