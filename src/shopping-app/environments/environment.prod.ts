9faaf670929066f2f48f9c83f17138d8c58d7f38
Hubert Popiolkiewicz <popiolkiewicz.it@gmail.com>
2020-05-13 08:26:16 +0200

    Deploying to firebase static web hosting service

--- src/shopping-app/environments/environment.prod.ts
+++ src/shopping-app/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyAjAigR8-_A9TzhJj5bhN9-UEQghu4SfC4'
 };
--- src/shopping-app/environments/environment.ts
+++ src/shopping-app/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyAjAigR8-_A9TzhJj5bhN9-UEQghu4SfC4'
 };
 
 /*
